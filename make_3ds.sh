5d8f546551b51b3e597c1ff515af5e3420cc14a0
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-01-15 21:55:36 +0000

    Add files via upload

---
+++ make_3ds.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TOOLCHAIN=/opt/armv6-eabihf--glibc--bleeding-edge-2020.02-2/bin/arm-buildroot-linux-gnueabihf-
+
+cp arch/arm/configs/nintendo3ds_defconfig .config
+
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j12
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN nintendo3ds_ctr.dtb
+
+echo "Output file: ./arch/arm/boot/zImage"
+echo "Output DTB: ./arch/arm/boot/dts/nintendo3ds_ctr.dtb"
